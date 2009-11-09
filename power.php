00454807f82ac1eafd45aac09e28076d0cdeac24
Pace Willisson <pace@alum.mit.edu>
2009-11-08 20:13:03 -0500

    tweaks

--- power.php
+++ power.php
@@ -4,8 +4,23 @@ require ("common.php");
 
 pstart ();
 
+$width = 0 + @$_REQUEST['width'];
+$height = 0 + @$_REQUEST['height'];
 
-$body .= "<canvas id='power_graph' width='640' height='480'></canvas>\n";
+if ($width == 0 || $height == 0) {
+	$width = 640;
+	$height = 480;
+}
+
+$body .= "<form action='power.php'>\n";
+$body .= sprintf ("width: <input type='text' name='width' value='%d' />\n",
+		  $width);
+$body .= sprintf ("width: <input type='text' name='height' value='%d' />\n",
+		  $height);
+$body .= "<input type='submit' value='Set' />\n";
+$body .= "</form>\n";
+
+$body .= "<canvas id='power_graph' width='$width' height='$height'></canvas>\n";
 
 pfinish ();
 ?>
